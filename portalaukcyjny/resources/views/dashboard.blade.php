ecad5f105dd24bb07c97af5b7d4a7aa7ca27da57
Wojtek Nowak <wojteknowak1122@gmail.com>
2023-11-19 23:22:19 +0100

    Dashboard

--- portalaukcyjny/resources/views/dashboard.blade.php
+++ portalaukcyjny/resources/views/dashboard.blade.php
@@ -1,17 +1,76 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/dashboard.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-    </div>
+
+  <main>
+  <div class="img_purchase">
+    <img src="storage/purchase1.jpg" alt="Purchase" class="img_purchase">
+  </div>
+    <section class="company-info">
+      <h4>SecondLife Marketplace - Twoje miejsce na sprzedaż i zakupy</h4>
+      <p>
+        SecondLife Marketplace to platforma internetowa, która umożliwia użytkownikom sprzedaż oraz zakup różnorodnych nowych i używanych przedmiotów. Nasza firma stawia na elastyczność oraz możliwość wyboru spośród szerokiej gamy produktów oferowanych przez naszych użytkowników.
+      </p>
+      <p>
+        Działamy na rzecz zapewnienia bezpiecznego i wygodnego środowiska dla osób poszukujących możliwości zarówno sprzedaży, jak i zakupu różnorodnych przedmiotów. Z nami każdy może znaleźć interesujące go produkty, bez względu na to, czy są to nowe inwestycje czy poszukiwania unikatowych rzeczy z drugiej ręki.
+      </p>
+      <a href="/products" class="btn">Rozpocznij Zakupy</a>
+    </section>
+  </main>
+
+<ol style="--length: 5" role="list">
+	<li style="--i: 1">
+		<h3>Różnorodność Produktów</h3>
+		<p>Na naszej platformie znajdziesz bogaty wybór różnych kategorii produktów, począwszy od elektroniki, ubrań, artykułów domowych, aż po unikatowe kolekcjonerskie przedmioty.</p>
+	</li>
+	<li style="--i: 2">
+		<h3>Nowe i Używane Towary</h3>
+		<p>Użytkownicy mogą wystawiać na sprzedaż zarówno zupełnie nowe, jak i starannie używane przedmioty. To pozwala naszym klientom wybierać zgodnie z własnymi potrzebami oraz budżetem.</p>
+	</li>
+	<li style="--i: 3">
+		<h3>Bezpieczne Transakcje</h3>
+		<p> Zapewniamy bezpieczne środowisko transakcyjne, wspierane przez zaawansowane metody płatności, co pozwala na pewne i niezawodne zakupy online.</p>
+	</li>
+	<li style="--i: 4">
+		<h3>Możliwość Wystawienia Produktu</h3>
+		<p>Każdy użytkownik może łatwo wystawić swój przedmiot na sprzedaż, umieszczając szczegółowy opis, zdjęcia oraz ustalając cenę i warunki sprzedaży.</p>
+	</li>
+	<li style="--i: 5">
+		<h3>Wsparcie dla Sprzedających i Kupujących</h3>
+		<p>Nasz zespół zapewnia wsparcie zarówno dla sprzedających, jak i kupujących, pomagając rozwiązywać wszelkie problemy czy pytania dotyczące transakcji.</p>
+	</li>
+</ol>
+
+<div class="footer">
+<div class="row">
+<a href="#"><i class="fa fa-facebook"></i></a>
+<a href="#"><i class="fa fa-instagram"></i></a>
+<a href="#"><i class="fa fa-youtube"></i></a>
+<a href="#"><i class="fa fa-twitter"></i></a>
+</div>
+
+<div class="grid-container">
+  <div class="grid-item"><a href="#">About us</a></div>
+  <div class="grid-item"><a href="#">Contact</a></div>
+  <div class="grid-item"><a href="#">FAQ</a></div>
+</div>
+
+<div class="row">
+Copyright © 2021 - All rights reserved 
+</div>
+</div>
+
+
+</body>
+
+</html>
+
 </x-app-layout>
